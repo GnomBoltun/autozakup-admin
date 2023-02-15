3103187631b25628248ffff2282e85a6db0c1ccf
gnomboltun <leevaa0@inbox.ru>
2023-02-15 12:21:17 +0600

    Sidebar finally

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,11 +1,27 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import DashboardCard from '../../Components/DasboardCard'
+import { People } from '@mui/icons-material'
 
 type Props = {}
 
-const index = (props: Props) => {
+const Dashboard = (props: Props) => {
   return (
-    <div>index</div>
+    <Grid gridRow={4} container spacing={4} sx={{margin: 0 ,marginRight: 4}}>
+      <Grid item xs={3}>
+        <DashboardCard title='Выигранные конкурсы' value={20} icon={<People/>} link='/' cardColor='cyan'/>
+      </Grid>
+      <Grid item xs={3}>
+        <DashboardCard title='Выигранные конкурсы' value={20} icon={<People/>} link='/' cardColor='cyan'/>
+      </Grid>
+      <Grid item xs={3}>
+        <DashboardCard title='Выигранные конкурсы' value={20} icon={<People/>} link='/' cardColor='cyan'/>
+      </Grid>
+      <Grid item xs={3}>
+        <DashboardCard title='Выигранные конкурсы' value={20} icon={<People/>} link='/' cardColor='cyan'/>
+      </Grid>
+    </Grid>
   )
 }
 
-export default index
+export default Dashboard
